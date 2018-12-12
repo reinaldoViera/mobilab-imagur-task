946e2ef32ff0c80da25fde444ad18807341ba7a2
Reinaldo Viera <rviera@redgage.com>
2018-12-12 15:48:22 -0500

    Loading album data from Album page.

--- src/component/Album.jsx
+++ src/component/Album.jsx
@@ -1,15 +1,45 @@
 import React from 'react'
 import AlbumImg from './AlbumImg';
 import AlbumDetails from './AlbumDetails';
+import { Button, CircularProgress, Typography, withStyles } from '@material-ui/core';
 
-export default function Album({ images = [], ups, downs, score, title }) {
+const styles = () => ({
+  status: {
+    left: '40%',
+    position: 'relative'
+  },
+  error: {
+    textAlign: 'center'
+  }
+});
+
+function Album({ images = [], ups, downs, score, title, error, loading, refreshAlbum, classes }) {
   return (
     <div>
-        <AlbumDetails ups={ups} downs={downs} score={score} title={title}/>
       {
-          images.length !== 0 && 
-          images.map((img) => (<AlbumImg key={img.id} {...img}/>))
+        images.length !== 0 ?
+          <div>
+            <AlbumDetails ups={ups} downs={downs} score={score} title={title} />
+            {images.map((img) => (<AlbumImg key={img.id} {...img} />))}
+          </div> :
+          <div>
+            {
+              error && <Typography className={classes.error} component="h1">
+                Error loading the Album, please try again
+                    </Typography>
+            }
+            {
+              loading ? <CircularProgress className={classes.status} /> :
+                <Button variant="contained" color="primary" className={classes.status} onClick={refreshAlbum}>
+                  Refresh
+                </Button>
+            }
+          </div>
       }
+
     </div>
   )
 }
+
+export default withStyles(styles)(Album)
+
